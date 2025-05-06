a2f445acc8a227f4e085fe501c23f5eb2a6c8f24
Julian Edwards <juledwar@cisco.com>
2025-05-06 13:48:34 +1000

    Simplify workflow tests

--- dbtesttools/tests/test_pgfixture_docker.py
+++
@@ -1,9 +0,0 @@
-from .test_pgfixture import TestPostgresContainer
-
-# Override the scenarios to only run Docker
-TestPostgresContainer.scenarios = [
-    ("docker", {"podman": False}),
-]
-
-# Optional: re-export the test class to make unittest discovery happy
-__all__ = ["TestPostgresContainer"]
--- dbtesttools/tests/test_pgfixture_podman.py
+++
@@ -1,9 +0,0 @@
-from .test_pgfixture import TestPostgresContainer
-
-# Override the scenarios to only run Podman
-TestPostgresContainer.scenarios = [
-    ("podman", {"podman": True}),
-]
-
-# Optional: re-export the test class to make unittest discovery happy
-__all__ = ["TestPostgresContainer"]
